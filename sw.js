353b26f094d69738c0b75db86e6eaf018838dbb2
vhbernardo11 <vhbernardo11@gmail.com>
2026-09-06 18:24:16 -0300

    chore: refresh cache for labeled demo samples

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='integratrampo-v6';
-const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+const CACHE='integratrampo-v7';
+const CORE=['./','./index.html','./styles.css','./app.js','./samples.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{
