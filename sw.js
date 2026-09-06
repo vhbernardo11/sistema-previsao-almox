42dc720534195e11c3773f190b27d49a812344e8
vhbernardo11 <vhbernardo11@gmail.com>
2026-09-06 18:06:05 -0300

    Atualiza cache da versão sem conteúdo de teste

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='integratrampo-v5';
-const CORE=['./','./index.html','./manifest.webmanifest'];
+const CACHE='integratrampo-v6';
+const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{
