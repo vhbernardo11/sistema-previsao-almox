74e86c67edde4ac3091101e9b0b775dff18d5d26
vhbernardo11 <vhbernardo11@gmail.com>
2026-09-08 10:34:29 -0300

    Refresh IntegraTrampo cache for Segurança category

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='integratrampo-v7';
+const CACHE='integratrampo-v8';
 const CORE=['./','./index.html','./styles.css','./app.js','./samples.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
